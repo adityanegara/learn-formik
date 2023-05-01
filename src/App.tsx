cd28e83352592016dcea49108e5a8326f4131c7d
Aditya Negara <aditnegarabisnis@gmail.com>
2023-05-01 22:01:58 +0700

    Learn until creating SignupForm

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// import Basic from './components/Basic';
+import Basic from './components/Overview/BasicLessBoilerplate';
+import SignupForm from './components/Tutorial/SignupForm';
+
+function App() {
+  return <SignupForm/>
+}
+
+export default App;
