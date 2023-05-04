09edf724eb362c08a93588904d5ab22c96fe2128
Aditya Negara <aditnegarabisnis@gmail.com>
2023-05-04 21:22:09 +0700

    Tutorial: Learn <Formik/> and its component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import Basic from './components/Basic';
 import Basic from './components/Overview/BasicLessBoilerplate';
-import SignupForm from './components/Tutorial/SignupForm';
+// import SignupForm from './components/Tutorial/SignupForm';
+// import SignupForm from './components/Tutorial/LeveragingReactContext';
+import SignupForm from './components/Tutorial/FieldComponent';
 
 function App() {
   return <SignupForm/>
